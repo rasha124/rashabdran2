374ea690f18e7de2e2a8610e0fb3139f3495b300
rasha124 <rasha12433@gmail.com>
2022-11-01 11:37:44 +0200

    aa

--- app/src/main/java/com/example/rashabdran/HomeActivity.java
+++ app/src/main/java/com/example/rashabdran/HomeActivity.java
@@ -16,27 +16,5 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
     }
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menuexample, menu);
-        return true;
-    }
 
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.sitting:
-                Toast.makeText(getApplicationContext(), "Item 1 Selected", Toast.LENGTH_LONG).show();
-                Intent i = new Intent(this, AboutActivity.class);
-                startActivity(i);
-                return true;
-            case R.id.about_menu:
-                Toast.makeText(getApplicationContext(), "Item 2 Selected", Toast.LENGTH_LONG).show();
-                return true;
-            case R.id.home:
-                Toast.makeText(getApplicationContext(), "Item 3 Selected", Toast.LENGTH_LONG).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
